ee3e1097f16b73c1d3558cbf9361b90c17e31216
Maria <mariak@prdxn.com>
2019-05-30 11:23:41 +0530

    project which lets you add quote and delete quote and limit no. of quotes to 10

---
+++ vue-quote-project/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <app-header
+      :quoteCount="quotes.length"
+      :maxQuotes="maxQuotes"
+    ></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid
+      :quotes="quotes"
+      @quoteDeleted="deleteQuote"
+    ></app-quote-grid>
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <div class="alert alert-info">
+          Info: click on the quote to delete it!
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
+export default {
+  data: function() {
+    return {
+      quotes: ["Just a quote"],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete some quotes first");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  }
+};
+</script>
+
+<style>
+</style>
