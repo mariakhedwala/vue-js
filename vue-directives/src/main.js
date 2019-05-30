1bb04c14d41fb2d68b0111558374c76e3423b77e
Maria <mariak@prdxn.com>
2019-05-30 11:25:43 +0530

    how to create your own derictive and use it

---
+++ vue-directives/src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    // el.style.backgroundColor = 'red';
+    // el.style.backgroundColor = binding.value;
+    // if (binding.arg == 'background') {
+    //   el.style.backgroundColor = binding.value;
+    // } else {
+    //   el.style.color = binding.value;
+    // }
+    var delay = 0;
+    if(binding.modifiers['delayed']) {
+      delay = 3000;
+    }
+    setTimeout(()=> {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
+  }
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
