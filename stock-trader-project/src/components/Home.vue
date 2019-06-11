8b4c4d4dc606a7b982f3571de1a4256e5c90b7cc
Maria <mariak@prdxn.com>
2019-06-11 12:24:25 +0530

    stock trader project upto portfolio

---
+++ stock-trader-project/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Trade or View your portfolio</h1>
+    <h6>You may save / load your data</h6>
+    <h6>Click on end day to begin a new day</h6>
+    <hr>
+    <p>Your funds: {{ funds }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    }
+  }
+};
+</script>
---
+++ stock-trader-project/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: "BMX", price: 110 },
+    { id: 1, name: "Google", price: 200 },
+    { id: 1, name: "Apple", price: 50 },
+    { id: 1, name: "Amazon", price: 200 }
+];
